7c8efd64354775e4fb6b28d717d1d42d4ce14124
Arvic S. Babol <kulotsystems@gmail.com>
2024-08-30 15:30:42 +0800

    00:15:28 - 03 Create a Layout File Using Laravel Components
    Do homework.

--- example/resources/views/components/layout.blade.php
+++ example/resources/views/components/layout.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About Page</title>
+    <title>Home Page</title>
 </head>
 <body>
-    <h1>Hello from the About Page.</h1>
+    <nav>
+        <x-nav-link href="/">Home</x-nav-link>
+        <x-nav-link href="/about">About</x-nav-link>
+        <x-nav-link href="/contact">Contact</x-nav-link>
+    </nav>
+    {{ $slot }}
 </body>
 </html>
