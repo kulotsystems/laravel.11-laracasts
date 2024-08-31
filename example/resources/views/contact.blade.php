d11ed48e736aba852476730cc86034a92582bd31
Arvic S. Babol <kulotsystems@gmail.com>
2024-08-31 17:14:28 +0800

    00:52:45 - 06 View Data and Route Wildcards

--- example/resources/views/contact.blade.php
+++ example/resources/views/contact.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <x-slot:heading>
-        Contact Page
+        About Page
     </x-slot:heading>
-    <h1>Hello from the Contact Page.</h1>
+    <h1>Hello from the About Page.</h1>
 </x-layout>
