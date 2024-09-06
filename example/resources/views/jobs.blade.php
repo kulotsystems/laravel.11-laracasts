7f21438b3380fbe96dc8097692da9315e90192a4
Arvic S. Babol <kulotsystems@gmail.com>
2024-09-06 12:47:29 +0800

    02:43:42 - 13 Eager Loading and the N+1 Problem

--- example/resources/views/jobs.blade.php
+++ example/resources/views/jobs.blade.php
@@ -2,13 +2,14 @@
     <x-slot:heading>
         Job Listings
     </x-slot:heading>
-    <ul>
+    <div class="space-y-4">
         @foreach($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500">{{ $job->employer->name }}</div>
+                <div>
                     <b>{{ $job['title'] }}:</b> Pays {{ $job['salary'] }} per year
-                </a>
-            </li>
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
--- example/routes/web.php
+++ example/routes/web.php
@@ -8,8 +8,11 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
+    // $jobs = Job::all();
+    $jobs = Job::with('employer')->get();
+
     return view('jobs', [
-        'jobs' => Job::all()
+        'jobs' => $jobs
     ]);
 });
 
